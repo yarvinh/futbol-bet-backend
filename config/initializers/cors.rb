256d545ca6d9ed4dad755bf40eb39eacb1a7fd61
Yarvin Hernandez <yarvinhhernandez@gmail.com>
2024-09-09 17:14:37 -0400

    add new origing in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-     origins 'http://localhost:3004','http://localhost:3001', 'http://localhost:3003', "http://10.0.0.6:3003"
+     origins 'http://localhost:3004','http://localhost:3001', 'http://localhost:3003', "http://10.0.0.6:3003","https://futbol-bet-561e73dd6e15.herokuapp.com"
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
